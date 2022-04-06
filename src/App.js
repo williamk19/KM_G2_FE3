64283e9d27de8c3d6bb91dce6ca2d9052e44e316
williamk19 <williamkurniawan1144@gmail.com>
2022-04-06 09:47:56 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Seacrh from './pages/search/index';
+import Trend from './pages/trending/index';
 import React, {useState} from 'react';
 import Gif from './components/gif.component';
 import './App.css';
 
-const API_KEY = '6II6Akr2GFYoZSooxoCE4PU9aJ88FDJ8';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 const App = () => {
   const [search, setSearch] = useState('');
   const [gifs, setGifs] = useState({});
@@ -14,7 +21,8 @@ const App = () => {
 
   const getGifs = (e) => {
     e.preventDefault();
-    const temp = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${search}&limit=12`;
+    const temp = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_KEY
+}&q=${search}&limit=12`;
     fetch(temp)
       .then((res) => res.json())
       .then(data => {
@@ -25,10 +33,35 @@ const App = () => {
 
   return (
     <>
-      <Seacrh getData={getGifs} getSearch={searchbarChange} />
-      {gifs.data?.map((gif) => (
-        <Gif title={gif.title} url={gif.images.fixed_width.url} key={gif.id}/>
-      ))}
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/search">Search</Link>
+              </li>
+              <li>
+                <Link to="/trend">Trend</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/search">
+              <Seacrh getData={getGifs} getSearch={searchbarChange} />
+              {gifs.data?.map((gif) => (
+                <Gif title={gif.title} url={gif.images.fixed_width.url} key={gif.id} />
+              ))}
+            </Route>
+            <Route path="/trend">
+              <Trend />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      
     </>
   );
 }
---
+++ src/pages/trending/index.js
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react';
+import Gif from '../../components/gif.component';
+
+
+
+const Trend = () => {
+    const [trend, setTrend] = useState({});
+
+    useEffect(() => {
+        const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_GIPHY_KEY}&limit=25&rating=g`;
+        fetch(url)
+            .then((res) => res.json())
+            .then(data => {
+                setTrend(data);
+            });
+        console.log(trend);
+    });
+
+    return (
+        <>
+            {trend.data?.map((gif) => (
+                <Gif title={gif.title} url={gif.images.fixed_width.url} key={gif.id} />
+            ))}
+        </>
+    );
+};
+
+export default Trend;
