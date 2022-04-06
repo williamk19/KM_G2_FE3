69a977f82dc5c247a4eaee722c98291e727c922f
williamk19 <williamkurniawan1144@gmail.com>
2022-04-06 08:49:19 +0700

    use hooks state

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
 import Seacrh from './pages/search/index';
-import React from 'react';
+import React, {useState} from 'react';
 import Gif from './components/gif.component';
 import './App.css';
 
 const API_KEY = '6II6Akr2GFYoZSooxoCE4PU9aJ88FDJ8';
-class App extends React.Component {
-  state = {
-    search: '',
-    gifs: {},
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [gifs, setGifs] = useState({});
+
+  const searchbarChange = (e) => {
+    setSearch(e.target.value);
   };
 
-  searchbarChange = (e) => {
-    this.setState({ search: e.target.value });
-  };
-
-  getGifs = (e) => {
+  const getGifs = (e) => {
     e.preventDefault();
-    const temp = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${this.state.search}&limit=12`;
+    const temp = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${search}&limit=12`;
     fetch(temp)
       .then((res) => res.json())
       .then(data => {
-        this.setState({ gifs: data });
+        setGifs(data);
       });
     
   };
 
-  render() {
-    return (
-      <>
-        <Seacrh getData={this.getGifs} getSearch={this.searchbarChange} />
-        {this.state.gifs.data?.map((gif) => (
-          <Gif title={gif.title} url={gif.images.fixed_width.url} key={gif.id}/>
-        ))}
-      </>
-    );
-  }
+  return (
+    <>
+      <Seacrh getData={getGifs} getSearch={searchbarChange} />
+      {gifs.data?.map((gif) => (
+        <Gif title={gif.title} url={gif.images.fixed_width.url} key={gif.id}/>
+      ))}
+    </>
+  );
 }
 
 export default App;
